a08a0be57e023887e83516e94c9201206499e01e
Tyro <ranajahid.cseru1@gmail.com>
2018-02-14 12:31:54 +0600

    Initialize the online registration apps component.

---
+++ app/src/main/java/com/example/tyro/firebaseregdemo/Regestration.java
@@ -0,0 +1,13 @@
+package com.example.tyro.firebaseregdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Regestration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regestration);
+    }
+}
